0c13f6ff271c34c1fa77f7a677c87ef75a8580a9
projases <140944318+projases@users.noreply.github.com>
2024-05-07 09:40:21 +0200

    delete subtitle and title components

--- src/components/hero/title/title.js
+++
@@ -1,8 +0,0 @@
-// import "./title.css";
-
-// export function title(importance, className, titleText) {
-//   const title = `
-//     <h${importance} class=${className}> ${titleText} </h1>
-//   `;
-//   return title;
-// }
